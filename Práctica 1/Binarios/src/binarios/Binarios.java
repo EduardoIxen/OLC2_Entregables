2f2ee0b32c61a025895faa71619a0041f1b7090f
Eduardo T. Ixen <tomas-1539@hotmail.com>
2022-01-24 17:17:48 -0600

    Practica 1

---
+++ Práctica 1/Binarios/src/binarios/Binarios.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package binarios;
+
+import java.io.FileInputStream;
+/**
+ *
+ * @author Eduardo-Ixen
+ */
+public class Binarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        interpretar("entrada.txt");
+    }
+    
+    private static void interpretar(String path){
+        analizadores.Parser parse;
+        try {
+            parse = new analizadores.Parser(new analizadores.Lexico(new FileInputStream(path)));
+            parse.parse();
+        } catch (Exception e) {
+            System.out.println("Error fatal en compilacion de entrada.");
+            System.out.println("Causa, "+ e.getCause());
+        }
+    }
+}
---
+++ Práctica 1/Palabras/src/analizadores/Compilar.bat
@@ -0,0 +1,4 @@
+javac Jlex/Main.java
+java JLex.Main Lexico
+java -jar Cup/java-cup-11b.jar -parser Parser Sintactico
+pause
